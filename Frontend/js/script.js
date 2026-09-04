863a2149d5a27f724fb8c3e73cf30326da29c55f
LUCCA MOTA SINDEAUX <04324341001@senacrs.edu.br>
2026-09-03 21:45:09 -0300

    commit inicial

---
+++ Frontend/js/script.js
@@ -0,0 +1,39 @@
+const listaCategorias = document.getElementById("listaCategorias");
+
+const URL = "http://localhost:8001"
+const endPointCategoria = URL + "/category"
+
+async function loadCategorias(){
+    try{
+        const resposta = await fetch(endPointCategoria)
+        if(!resposta.ok){
+            alert("Erro!")
+            return
+        }
+        const categorias = await resposta.json()
+        listaCategorias.innerHTML = ""
+
+        categorias.forEach(cat => {
+            listaCategorias.innerHTML += 
+            `
+                <tr>
+                    <td>${cat.id}</td>
+                    <td>${cat.nome}</td>
+                    <td>
+                        <button class = "btn btn-info" onclick = "preencherForm( ${cat.id}, ${cat.nome} )">
+                            Editar
+                        </button>
+                        <button class = "btn btn-danger" onclick = "excluirCategoria( ${cat.id} )">
+                            Excluir
+                        </button>
+                    </td>
+                </tr>
+            `
+        });
+    }catch(erro){
+        console.error(erro)
+        alert("Erro ao carregar categorias.")
+    } 
+}
+
+loadCategorias();
